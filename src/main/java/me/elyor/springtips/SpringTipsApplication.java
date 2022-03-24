9b9fb9c598acbd2b679306fe332db5d92034b32e
Elyorbek Ibrokhimov <eliyoribek@gmail.com>
2022-03-24 17:27:31 +0900

    Initial commit

---
+++ src/main/java/me/elyor/springtips/SpringTipsApplication.java
@@ -0,0 +1,13 @@
+package me.elyor.springtips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTipsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTipsApplication.class, args);
+    }
+
+}
