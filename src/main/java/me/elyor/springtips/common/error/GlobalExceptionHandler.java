5ca441eaba487ab8ef9ffda2aa02ab5c45a56f34
Elyorbek Ibrokhimov <eliyoribek@gmail.com>
2022-03-29 11:36:10 +0900

    Add validation exception handler

--- src/main/java/me/elyor/springtips/common/error/GlobalExceptionHandler.java
+++ src/main/java/me/elyor/springtips/common/error/GlobalExceptionHandler.java
@@ -1,20 +1,36 @@
 package me.elyor.springtips.common.error;
 
+import me.elyor.springtips.common.error.exception.ErrorCode;
 import me.elyor.springtips.common.error.exception.GlobalException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+@RestControllerAdvice
+public class GlobalExceptionHandler {
     private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
+    /**
+     *  Handles all the business logic exceptions
+     */
     @ExceptionHandler(GlobalException.class)
     public ResponseEntity<ErrorResponse> globalHandler(GlobalException exception) {
         logger.error("GlobalException", exception);
         final ErrorResponse errorResponse = exception.getErrorResponse();
+        return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(errorResponse.getStatus()));
+    }
+
+    /**
+     *  Handles the cases for javax.validation.Valid or @Validated
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+        logger.error("MethodArgumentNotValidException", ex);
+        ErrorResponse errorResponse = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE, ex.getBindingResult());
         return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(errorResponse.getStatus()));
     }
 }
